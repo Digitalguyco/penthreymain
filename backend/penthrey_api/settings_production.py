27cac6ce0d52446c149ed0ba8f5a94ad455b5959
Digitalguyco <youngkhito@gmail.com>
2025-08-21 17:47:47 +0100

    static change

--- backend/penthrey_api/settings_production.py
+++ backend/penthrey_api/settings_production.py
@@ -34,11 +34,11 @@ DATABASES = {
 # Static files configuration for production
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),os.path.join(BASE_DIR, 'staticfiles')]
 
 # Media files configuration
 MEDIA_URL = '/media/'
-MEDIA_ROOT =[os.path.join(BASE_DIR, 'media'),]
+MEDIA_ROOT =[os.path.join(BASE_DIR, 'media'),os.path.join(BASE_DIR, 'mediafiles')]
 
 # Security settings for production
 SECURE_BROWSER_XSS_FILTER = True
