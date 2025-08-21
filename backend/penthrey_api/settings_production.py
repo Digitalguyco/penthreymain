cc28fa67b6e7689d8823db38d908be344a0a3eff
Digitalguyco <youngkhito@gmail.com>
2025-08-21 18:22:46 +0100

    white noise

--- backend/penthrey_api/settings_production.py
+++ backend/penthrey_api/settings_production.py
@@ -15,8 +15,7 @@ ALLOWED_HOSTS = [
     'www.penthrey.com',
 ]
 
-# WhiteNoise settings
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Database configuration for MySQL
 DATABASES = {
@@ -72,6 +71,9 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@your-domain.com')
+
+# WhiteNoise settings
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Logging configuration
 LOGGING = {
